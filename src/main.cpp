eb618e3d867136a7b8a50eaef1bd3fec60a9d107
GiverPlay007 <willyson.vilela@gmail.com>
2022-10-15 16:14:18 -0300

    Create OpenGL context

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,50 @@
 #include <iostream>
-
-#include <GLFW/glfw3.h>
 #include <cassert>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 const int width = 1280;
 const int height = 720;
 
+void print_gl_version();
+
 int main()
 {
-  assert(glfwInit( ));
+  assert(glfwInit() == GLFW_TRUE);
 
   GLFWwindow* window = glfwCreateWindow(width, height, "Blue Marble", nullptr, nullptr);
   assert(window);
 
+  glfwMakeContextCurrent(window);
+  assert(glewInit() == GLEW_OK);
+
+  print_gl_version();
+
+  glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+
   while(!glfwWindowShouldClose(window))
   {
+    glClear(GL_COLOR_BUFFER_BIT);
     glfwPollEvents();
     glfwSwapBuffers(window);
   }
 
   glfwTerminate();
   return 0;
+}
+
+void print_gl_version()
+{
+  GLint glMajorVersion = 0;
+  GLint glMinorVersion = 0;
+
+  glGetIntegerv(GL_MAJOR_VERSION, &glMajorVersion);
+  glGetIntegerv(GL_MINOR_VERSION, &glMinorVersion);
+
+  std::cout << "OpenGL Version: " << glMajorVersion << "." << glMajorVersion << std::endl;
+  std::cout << "OpenGl Vendor: " << glGetString(GL_VENDOR) << std::endl;
+  std::cout << "OpenGl Renderer: " << glGetString(GL_RENDERER) << std::endl;
+  std::cout << "OpenGl Version (renderer): " << glGetString(GL_VERSION) << std::endl;
+  std::cout << "GLSL Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
 }
