9e96a08af0ff1740c06a4a61babbbdfa73071737
GiverPlay007 <willyson.vilela@gmail.com>
2022-10-15 15:26:06 -0300

    Initialize GLFW and create window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+#include <cassert>
+
+const int width = 1280;
+const int height = 720;
+
 int main()
-{ 
-  std::cout << "Hello, world!" << std::endl;
+{
+  assert(glfwInit( ));
+
+  GLFWwindow* window = glfwCreateWindow(width, height, "Blue Marble", nullptr, nullptr);
+  assert(window);
+
+  while(!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+  }
+
+  glfwTerminate();
   return 0;
 }
